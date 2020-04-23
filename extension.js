6a4fa7cdfcdfb859e3eb2215c4cea9ad10b2ee24
Harsh Lakhanpal <harshlakhanpaln@gmail.com>
2020-04-23 20:07:19 +0530

    Language(JS) specific snippets

--- extension.js
+++ extension.js
@@ -1,6 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+const { v4: uuidv4 } = require("uuid");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,10 +21,42 @@ function activate(context) {
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "code-injector.helloWorld",
-    function() {
+    "code-injector.ci",
+
+    async function () {
+      let cifilepath = `${__dirname}/snippets.json`;
       const textEditor = vscode.window.activeTextEditor;
-      console.log(textEditor);
+      let document = textEditor.document;
+      let selection = textEditor.selection;
+
+      let selectedCode = document.getText(selection);
+
+      let snippetPrefixInput = {
+        prompt: "Set a prefix for your snippet",
+        placeHolder: "Prefix",
+      };
+
+      let snippet = {};
+      let snippetsObject = JSON.parse(fs.readFileSync(cifilepath, "utf8"));
+
+      // fs.readFile(cifilepath, (data) => snippetsObject = data );
+      // console.log(snippetsObject);
+
+      if (selectedCode.length > 0) {
+        console.log(selectedCode);
+        snippet.prefix = await vscode.window.showInputBox(snippetPrefixInput);
+        snippet.name = uuidv4();
+
+        snippetsObject[snippet.name] = {
+          prefix: [`ci-${snippet.prefix}`, snippet.prefix],
+          body: snippetBody(selectedCode),
+          description: selectedCode,
+        };
+
+        let newSnippet = JSON.stringify(snippetsObject, null, "\t");
+        fs.writeFileSync(cifilepath, newSnippet, "utf8");
+      } else console.log("please select some code ");
+      // console.log(__dirname);
     }
   );
 
@@ -28,10 +64,15 @@ function activate(context) {
 }
 exports.activate = activate;
 
+const snippetBody = (content) => {
+  let body = content.replace(/\t/g, "\\t");
+  return body.split("\n");
+};
+
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
   activate,
-  deactivate
+  deactivate,
 };
